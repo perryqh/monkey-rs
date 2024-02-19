1e5283d7952cc865b7e957a4b8397c269f5c8716
Perry Hertler <perry.hertler@gusto.com>
2024-02-18 20:56:50 -0600

    handling != and ==

--- lexer/src/lexer.rs
+++ lexer/src/lexer.rs
@@ -67,7 +67,14 @@ impl<'a> Lexer<'a> {
         self.skip_whitespace();
         let next_char = self.chars.next()?;
         let token = match next_char {
-            '=' => Token::new(TokenKind::Assign),
+            '=' => {
+                if let Some(&'=') = self.chars.peek() {
+                    self.chars.next();
+                    Token::new(TokenKind::EQ)
+                } else {
+                    Token::new(TokenKind::Assign)
+                }
+            }
             ';' => Token::new(TokenKind::Semicolon),
             '(' => Token::new(TokenKind::LParen),
             ')' => Token::new(TokenKind::RParen),
@@ -77,7 +84,14 @@ impl<'a> Lexer<'a> {
             '}' => Token::new(TokenKind::RBrace),
             '[' => Token::new(TokenKind::LBracket),
             ']' => Token::new(TokenKind::RBracket),
-            '!' => Token::new(TokenKind::Bang),
+            '!' => {
+                if let Some(&'=') = self.chars.peek() {
+                    self.chars.next();
+                    Token::new(TokenKind::NotEq)
+                } else {
+                    Token::new(TokenKind::Bang)
+                }
+            }
             '-' => Token::new(TokenKind::Minus),
             '*' => Token::new(TokenKind::Asterisk),
             '/' => Token::new(TokenKind::Slash),
@@ -147,6 +161,129 @@ mod tests {
             TokenKind::Plus,
             TokenKind::INT(5),
             TokenKind::Semicolon,
+            TokenKind::EOF,
+        ];
+        let mut lexer = Lexer::new(input);
+        for kind in expected_tokens {
+            let token = lexer.next().unwrap();
+            assert_eq!(token.kind, kind);
+        }
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_next_token_with_identifiers() {
+        let input = "let five = 5; let ten = 10; let add = fn(x, y) { x + y; };";
+        let expected_tokens = vec![
+            TokenKind::Let,
+            TokenKind::Identifier {
+                name: "five".to_string(),
+            },
+            TokenKind::Assign,
+            TokenKind::INT(5),
+            TokenKind::Semicolon,
+            TokenKind::Let,
+            TokenKind::Identifier {
+                name: "ten".to_string(),
+            },
+            TokenKind::Assign,
+            TokenKind::INT(10),
+            TokenKind::Semicolon,
+            TokenKind::Let,
+            TokenKind::Identifier {
+                name: "add".to_string(),
+            },
+            TokenKind::Assign,
+            TokenKind::Function,
+            TokenKind::LParen,
+            TokenKind::Identifier {
+                name: "x".to_string(),
+            },
+            TokenKind::Comma,
+            TokenKind::Identifier {
+                name: "y".to_string(),
+            },
+            TokenKind::RParen,
+            TokenKind::LBrace,
+            TokenKind::Identifier {
+                name: "x".to_string(),
+            },
+            TokenKind::Plus,
+            TokenKind::Identifier {
+                name: "y".to_string(),
+            },
+            TokenKind::Semicolon,
+            TokenKind::RBrace,
+            TokenKind::Semicolon,
+            TokenKind::EOF,
+        ];
+        let mut lexer = Lexer::new(input);
+        for kind in expected_tokens {
+            let token = lexer.next().unwrap();
+            assert_eq!(token.kind, kind);
+        }
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_next_token_with_multi_char_operators() {
+        let input = "if (5 != 10) { return true; } else if (3 == 3) { return false; }";
+        let expected_tokens = vec![
+            TokenKind::If,
+            TokenKind::LParen,
+            TokenKind::INT(5),
+            TokenKind::NotEq,
+            TokenKind::INT(10),
+            TokenKind::RParen,
+            TokenKind::LBrace,
+            TokenKind::Return,
+            TokenKind::True,
+            TokenKind::Semicolon,
+            TokenKind::RBrace,
+            TokenKind::Else,
+            TokenKind::If,
+            TokenKind::LParen,
+            TokenKind::INT(3),
+            TokenKind::EQ,
+            TokenKind::INT(3),
+            TokenKind::RParen,
+            TokenKind::LBrace,
+            TokenKind::Return,
+            TokenKind::False,
+            TokenKind::Semicolon,
+            TokenKind::RBrace,
+            TokenKind::EOF,
+        ];
+        let mut lexer = Lexer::new(input);
+        for kind in expected_tokens {
+            let token = lexer.next().unwrap();
+            assert_eq!(token.kind, kind);
+        }
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_next_token_with_all_operators() {
+        let input = "=+(){},;![]-*/<>==!=";
+        let expected_tokens = vec![
+            TokenKind::Assign,
+            TokenKind::Plus,
+            TokenKind::LParen,
+            TokenKind::RParen,
+            TokenKind::LBrace,
+            TokenKind::RBrace,
+            TokenKind::Comma,
+            TokenKind::Semicolon,
+            TokenKind::Bang,
+            TokenKind::LBracket,
+            TokenKind::RBracket,
+            TokenKind::Minus,
+            TokenKind::Asterisk,
+            TokenKind::Slash,
+            TokenKind::LT,
+            TokenKind::GT,
+            TokenKind::EQ,
+            TokenKind::NotEq,
             TokenKind::EOF,
         ];
         let mut lexer = Lexer::new(input);
